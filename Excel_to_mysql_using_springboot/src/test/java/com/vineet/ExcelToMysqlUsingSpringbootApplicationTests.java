13d9efc6597e5e6ec6e386cb0a29d65b71575953
Mr. Vineet Singh <Mr. Vineet Singh@vineet-221713>
2023-03-30 13:59:43 +0530

    This is frist commit with coad

---
+++ Excel_to_mysql_using_springboot/src/test/java/com/vineet/ExcelToMysqlUsingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelToMysqlUsingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
